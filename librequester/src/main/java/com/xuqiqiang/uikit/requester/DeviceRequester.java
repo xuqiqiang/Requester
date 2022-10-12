f46970d01b1c2ac21bb5c277a398fc50dfe9efa8
xuqiqiang <1239749058@qq.com>
2022-10-12 20:15:50 +0800

    Add: DeviceRequester

--- librequester/src/main/java/com/xuqiqiang/uikit/requester/DeviceRequester.java
+++ librequester/src/main/java/com/xuqiqiang/uikit/requester/DeviceRequester.java
@@ -3,14 +3,20 @@ package com.xuqiqiang.uikit.requester;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.location.LocationManager;
+import android.text.TextUtils;
 import com.xuqiqiang.uikit.requester.proxy.DeviceActivity;
+import com.xuqiqiang.uikit.view.ToastMaster;
 
 public class DeviceRequester {
 
     public static final int DEVICE_BLUETOOTH_ENABLE = 1;
     public static final int DEVICE_BLUETOOTH_DISABLE = 2;
+    public static final int DEVICE_LOCATION_ENABLE = 3;
+    public static final int DEVICE_LOCATION_DISABLE = 4;
 
     private static BluetoothAdapter mBluetoothAdapter;
+    private static LocationManager mLocationManager;
 
     public static boolean isSupportBle(Context context) {
         return context.getPackageManager().hasSystemFeature("android.hardware.bluetooth_le");
@@ -21,7 +27,7 @@ public class DeviceRequester {
         if (mBluetoothAdapter == null) {
             mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         }
-        return mBluetoothAdapter.isEnabled();
+        return mBluetoothAdapter != null && mBluetoothAdapter.isEnabled();
     }
 
     public static void enableBluetooth(Context context, OnDeviceListener listener) {
@@ -42,6 +48,42 @@ public class DeviceRequester {
             return;
         }
         DeviceActivity.start(context, DEVICE_BLUETOOTH_DISABLE, listener);
+    }
+
+    public static boolean isLocationEnable(Context context) {
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        }
+        boolean isGPS = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetwork = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        return isGPS || isNetwork;
+    }
+
+    public static void enableLocation(Context context, OnDeviceListener listener) {
+        enableLocation(context, listener, null);
+    }
+
+    public static void enableLocation(Context context, OnDeviceListener listener, String prompt) {
+        if (isLocationEnable(context)) {
+            if (listener != null) {
+                listener.onRequest(true);
+            }
+            return;
+        }
+        DeviceActivity.start(context, DEVICE_LOCATION_ENABLE, listener);
+        if (!TextUtils.isEmpty(prompt)) {
+            ToastMaster.showToast(context, prompt);
+        }
+    }
+
+    public static void disableLocation(Context context, OnDeviceListener listener) {
+        if (!isLocationEnable(context)) {
+            if (listener != null) {
+                listener.onRequest(true);
+            }
+            return;
+        }
+        DeviceActivity.start(context, DEVICE_LOCATION_DISABLE, listener);
     }
 
     public interface OnDeviceListener {
--- librequester/src/main/java/com/xuqiqiang/uikit/requester/proxy/DeviceActivity.java
+++ librequester/src/main/java/com/xuqiqiang/uikit/requester/proxy/DeviceActivity.java
@@ -5,20 +5,26 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import com.xuqiqiang.uikit.requester.DeviceRequester;
 
 import static com.xuqiqiang.uikit.requester.DeviceRequester.DEVICE_BLUETOOTH_DISABLE;
 import static com.xuqiqiang.uikit.requester.DeviceRequester.DEVICE_BLUETOOTH_ENABLE;
+import static com.xuqiqiang.uikit.requester.DeviceRequester.DEVICE_LOCATION_DISABLE;
+import static com.xuqiqiang.uikit.requester.DeviceRequester.DEVICE_LOCATION_ENABLE;
 
 public class DeviceActivity extends Activity {
     private static final int REQUEST_DEVICE = 0x004001;
     private static final String PARAM_TYPE = "PARAM_TYPE";
     private static final String
-        ACTION_REQUEST_BLUETOOTH_DISABLE = "android.bluetooth.adapter.action.REQUEST_DISABLE";
+            ACTION_REQUEST_BLUETOOTH_DISABLE = "android.bluetooth.adapter.action.REQUEST_DISABLE";
     private static DeviceRequester.OnDeviceListener mOnDeviceListener;
+    private int mType;
+    private boolean isJumpToSettingPage;
+    private long mJumpTime;
 
     public static void start(final Context context, final int type,
-        final DeviceRequester.OnDeviceListener listener) {
+                             final DeviceRequester.OnDeviceListener listener) {
         mOnDeviceListener = listener;
         Intent intent = new Intent(context, DeviceActivity.class);
         intent.putExtra(PARAM_TYPE, type);
@@ -32,16 +38,27 @@ public class DeviceActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        int type = intent.getIntExtra(PARAM_TYPE, DEVICE_BLUETOOTH_ENABLE);
+        mType = intent.getIntExtra(PARAM_TYPE, DEVICE_BLUETOOTH_ENABLE);
         Intent contentIntent = null;
-        if (type == DEVICE_BLUETOOTH_ENABLE) {
+        if (mType == DEVICE_BLUETOOTH_ENABLE) {
             contentIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        } else if (type == DEVICE_BLUETOOTH_DISABLE) {
+        } else if (mType == DEVICE_BLUETOOTH_DISABLE) {
             contentIntent = new Intent(ACTION_REQUEST_BLUETOOTH_DISABLE);
+        } else if (mType == DEVICE_LOCATION_ENABLE || mType == DEVICE_LOCATION_DISABLE) {
+            contentIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            contentIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         }
+        boolean start = false;
         if (contentIntent != null) {
-            startActivityForResult(contentIntent, REQUEST_DEVICE);
-        } else {
+            try {
+                startActivityForResult(contentIntent, REQUEST_DEVICE);
+                start = true;
+                isJumpToSettingPage = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (!start) {
             if (mOnDeviceListener != null) {
                 mOnDeviceListener.onRequest(false);
             }
@@ -51,14 +68,41 @@ public class DeviceActivity extends Activity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_DEVICE) {
+        if (requestCode == REQUEST_DEVICE
+                && (mType == DEVICE_BLUETOOTH_ENABLE || mType == DEVICE_BLUETOOTH_DISABLE)) {
             if (mOnDeviceListener != null) {
-                mOnDeviceListener.onRequest(
-                    resultCode == Activity.RESULT_OK);
+                mOnDeviceListener.onRequest(resultCode == Activity.RESULT_OK);
             }
             finish();
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isJumpToSettingPage && mJumpTime > 0 && System.currentTimeMillis() - mJumpTime > 100) {
+            if (mType == DEVICE_LOCATION_ENABLE || mType == DEVICE_LOCATION_DISABLE) {
+                boolean result = false;
+                if (mType == DEVICE_LOCATION_ENABLE) {
+                    result = DeviceRequester.isLocationEnable(this);
+                } else if (mType == DEVICE_LOCATION_DISABLE) {
+                    result = !DeviceRequester.isLocationEnable(this);
+                }
+                if (mOnDeviceListener != null) {
+                    mOnDeviceListener.onRequest(result);
+                }
+                finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isJumpToSettingPage && mJumpTime == 0) {
+            mJumpTime = System.currentTimeMillis();
+        }
     }
 
     @Override
